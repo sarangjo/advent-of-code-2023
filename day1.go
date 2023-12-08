92d8ffe32f9aec8076a137997a4355bb16c24a3f
Sarang Joshi <sarangj@msn.com>
2023-12-07 19:02:04 -0600

    Expanding to pt 2 but failing

--- day1.go
+++ day1.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"unicode"
 )
 
@@ -24,6 +25,61 @@ func processLine(line string) int {
 	return first*10 + last
 }
 
+var digitWords = []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+func processLine2(line string) int {
+	var first, last int
+	var translated int
+	seenFirst := false
+	active := make([]string, 10)
+	for _, c := range line {
+		// As we process each letter, we either:
+		// - complete a number
+		// - update possible numbers
+		if unicode.IsNumber(c) {
+			translated = int(c - '0')
+			if !seenFirst {
+				first = translated
+				seenFirst = true
+			}
+			last = translated
+			continue
+		}
+		for i, digit := range digitWords {
+			if strings.Index(digit, active[i]+string(c)) == 0 {
+				active[i] += string(c)
+				if active[i] == digit {
+					// Found!
+					translated = i
+					// Clear out anyone else who might be having ideas about starting early
+					if !seenFirst {
+						first = translated
+						seenFirst = true
+					}
+					last = translated
+					// Thank god no digit starts with the same letter as its last letter
+					active[i] = ""
+				}
+			} else {
+				active[i] = ""
+			}
+		}
+		// Creating a common section to save either the numeric values or the text-based values; they
+		// both jump here
+		// save:
+		// 	if saving {
+		// 		// active = make([]string, 10)
+		// 		if !seenFirst {
+		// 			first = translated
+		// 			seenFirst = true
+		// 		}
+		// 		last = translated
+		// 		saving = false
+		// 	}
+	}
+	return first*10 + last
+}
+
 func main() {
 	// https://adventofcode.com/2023/day/1/input
 	file, err := os.Open("day1.txt")
@@ -33,8 +89,15 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	sum := 0
+	idx := 0
 	for scanner.Scan() {
-		sum += processLine(scanner.Text())
+		lineRes := processLine2(scanner.Text())
+		fmt.Println("lineRes", lineRes)
+		sum += lineRes
+		// if idx > 15 {
+		// 	break
+		// }
+		idx++
 	}
 
 	fmt.Println(sum)
