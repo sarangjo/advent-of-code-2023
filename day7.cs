e4ac3e9931c4e8d9c390bdc6aa46660adc88be53
Sarang Joshi <sarangj@msn.com>
2024-01-25 02:52:00 -0600

    Start Day 7

---
+++ day7.cs
@@ -0,0 +1,12 @@
+class day7
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadLines("sample7.txt");
+        foreach (var line in lines)
+        {
+            string[] parts = line.Split(' ');
+            Dictionary<string, int> hand = [];
+        }
+    }
+}
