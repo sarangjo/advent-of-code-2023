6994b22caf203bdd0204b271c831f8ae510e0038
Sarang Joshi <sarangj@msn.com>
2024-02-02 00:54:30 -0600

    Start on part 2

--- day10.rb
+++ day10.rb
@@ -1,71 +1,142 @@
-file = File.open("day10.txt")
+require "set"
 
-# `exclude` indicates which direction we want to exclude even if there's a pipe there.
-Location = Struct.new(:row, :col, :exclude)
+file = File.open("sample10.txt")
 
-coords = Location.new(-1, -1, 'C')
+# `from` indicates which direction came from
+Location = Struct.new(:row, :col, :from)
+
 $lines = file.readlines.map(&:chomp)
 
-# Start by finding S
-$lines.each_with_index { |val, index|
-    col = val.index('S')
-    if col != nil then
-        coords.row = index
-        coords.col = col
-        break
+def go_in_dir(loc, dir)
+    if dir == 'U' then
+        return Location.new(loc.row - 1, loc.col, 'D')
+    elsif dir == 'D' then
+        return Location.new(loc.row + 1, loc.col, 'U')
+    elsif dir == 'L' then
+        return Location.new(loc.row, loc.col - 1, 'R')
+    else # R
+        return Location.new(loc.row, loc.col + 1, 'L')
     end
-}
+end
 
-def find_adj(coords)
+def leave_start(loc)
     adj = []
-    if coords.exclude != 'U' and coords.row != 0 then
-        up = $lines[coords.row - 1][coords.col]
-        if up == '7' or up == '|' or up == 'F' then
-            adj.append(Location.new(coords.row - 1, coords.col, 'D'))
-        end
+    if loc.row != 0 and '7|F'.include?($lines[loc.row - 1][loc.col]) then
+        adj.append(go_in_dir(loc, 'U'))
     end
-    if coords.exclude != 'D' and coords.row != $lines.length() - 1 then
-        down = $lines[coords.row + 1][coords.col]
-        if down == 'J' or down == '|' or down == 'L' then
-            adj.append(Location.new(coords.row + 1, coords.col, 'U'))
-        end
+    if loc.row != $lines.length() - 1 and 'J|L'.include?($lines[loc.row + 1][loc.col]) then
+        adj.append(go_in_dir(loc, 'D'))
     end
-    if coords.exclude != 'L' and coords.col != 0 then
-        left = $lines[coords.row][coords.col-1]
-        if left == 'L' or left == '-' or left == 'F' then
-            adj.append(Location.new(coords.row, coords.col - 1, 'R'))
-        end
+    if loc.col != 0 and 'L-F'.include?($lines[loc.row][loc.col-1]) then
+        adj.append(go_in_dir(loc, 'L'))
     end
-    if coords.exclude != 'R' and coords.col != $lines[coords.row].length - 1 then
-        right = $lines[coords.row][coords.col+1]
-        if right == 'J' or right == '-' or right == '7' then
-            adj.append(Location.new(coords.row, coords.col + 1, 'L'))
-        end
+    if loc.col != $lines[loc.row].length - 1 and 'J-7'.include?($lines[loc.row][loc.col+1]) then
+        adj.append(go_in_dir(loc, 'R'))
     end
     return adj
 end
 
-def coord_eq(adj)
-    return (adj[0].row == adj[1].row and adj[0].col == adj[1].col)
+$follow_pipe = {
+    '|' => ['U', 'D'],
+    '-' => ['L', 'R'],
+    'L' => ['U', 'R'],
+    'J' => ['U', 'L'],
+    '7' => ['D', 'L'],
+    'F' => ['D', 'R'],
+}
+
+# Follow the pipe, avoiding loc.from
+def find_next(loc)
+    dirs = $follow_pipe[$lines[loc.row][loc.col]]
+    return go_in_dir(loc, dirs[0] == loc.from ? dirs[1] : dirs[0])
 end
 
-puts(coords)
-
-# Okay excellent, now we need to traverse the loop starting at S to find where we meet again
-# Fencepost because the answer can never be 1
-steps = 1
-adj = find_adj(coords)
-puts(steps)
-puts(adj)
-
-while not coord_eq(adj) do
-    adj[0] = find_adj(adj[0])[0]
-    adj[1] = find_adj(adj[1])[0]
-
-    steps += 1
-
-    puts(steps)
-    puts(adj)
+def loc_eq(locs)
+    return (locs[0].row == locs[1].row and locs[0].col == locs[1].col)
 end
 
-print "Steps: " + steps.to_s
+# Start by finding S
+$start = Location.new(-1, -1, 'C')
+$lines.each_with_index { |val, index|
+    col = val.index('S')
+    if col != nil then
+        $start.row = index
+        $start.col = col
+        break
+    end
+}
+
+def part_1
+    # Okay excellent, now we need to traverse the loop starting at S to find where we meet again
+    # Fencepost because the answer can never be 1
+    steps = 1
+    locs = leave_start($start)
+
+    while not loc_eq(locs) do
+        locs[0] = find_next(locs[0])
+        locs[1] = find_next(locs[1])
+
+        steps += 1
+    end
+
+    print "Steps: " + steps.to_s
+end
+
+def part_2
+    # First, we collect the pipe locations row-wise
+    pipe = {}
+
+    # Add start
+    locs = leave_start($start)
+
+    # Based on locs, figure out what start should be
+    dirs = locs.map
+
+    pipe[locs[0].row] = pipe.has_key?(locs[0].row) ? pipe[locs[0].row].add(locs[0].col) : SortedSet.new([locs[0].col])
+    pipe[locs[1].row] = pipe.has_key?(locs[1].row) ? pipe[locs[1].row].add(locs[1].col) : SortedSet.new([locs[1].col])
+
+    pipe[$start.row] = SortedSet.new([$start.col])
+
+    while not loc_eq(locs) do
+        locs[0] = find_next(locs[0])
+        locs[1] = find_next(locs[1])
+
+        pipe[locs[0].row] = pipe.has_key?(locs[0].row) ? pipe[locs[0].row].add(locs[0].col) : SortedSet.new([locs[0].col])
+        pipe[locs[1].row] = pipe.has_key?(locs[1].row) ? pipe[locs[1].row].add(locs[1].col) : SortedSet.new([locs[1].col])
+    end
+
+    pipe = pipe.sort_by { |key| key }.to_h
+
+    # Now we evaluate how many spaces are in between by row
+    inside_spots = 0
+    is_first = true
+    row_count = 0
+    pipe.each { |key, row|
+        if is_first then
+            is_first = false
+            next
+        end
+
+        inside = false
+        prev = -1
+        row.each { |col|
+            if inside then
+                row_count += (col - prev - 1)
+            end
+
+            inside = !inside
+            prev = col
+        }
+
+        inside_spots += row_count
+        row_count = 0
+    }
+
+    # Remove the last row
+    inside_spots -= row_count
+
+    puts "Inside spots: " + inside_spots.to_s
+end
+
+# part_1
+part_2
