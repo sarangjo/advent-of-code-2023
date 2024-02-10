0e0da97bdc33a163a8261a49d1641fb607dcc132
Sarang Joshi <sarangj@msn.com>
2024-02-10 02:49:59 -0600

    Finish day 12 part 1

--- day12.kt
+++ day12.kt
@@ -1,16 +1,16 @@
 import java.io.File
 
-fun part1(): Int {
+fun part1(fn: String): Int {
     var sum = 0
 
-    val f = File("sample12.txt")
+    val f = File(fn)
     f.forEachLine(action = fun(line: String) {
         // Split into configuration and mapping
         val parts = line.split(" ")
         val springs = parts[0]
         val segments = parts[1].split(",").map { it.toInt() }
 
-        println(springs)
+        println("Line: $line")
         val opts = processLine("", springs, segments.toMutableList())
         println(opts)
 
@@ -25,10 +25,13 @@ fun processLine(parsed: String, line: String, segments: MutableList<Int>): Int {
     // Recursively process line based on whether this is a ? or not
     // What if we run out of segments?
     if (segments.size == 0) {
-        return if (line.contains('#')) 0 else 1
+        if (!line.contains('#')) {
+            // println(parsed + line)
+            return 1
+        }
+        return  0
     }
-    var curSegment = segments[0]
-
+    val curSegment = segments[0]
     var sum = 0
 
     if (line.length < curSegment) {
@@ -36,37 +39,29 @@ fun processLine(parsed: String, line: String, segments: MutableList<Int>): Int {
     }
     val potentialLineSegment = line.substring(0, curSegment)
     if (!potentialLineSegment.contains('.')) {
-        // We are now consuming the next segment!
-        var consumedParsed = parsed
-        var consumedCurSegment = curSegment
+        // Okay, our segment matches. But we can only move forward if the next element is not '#'.
+        if (line.length == curSegment || line[curSegment] != '#') {
+            // If we're not at the end, we additionally consume an extra character
+            val notEnd = line.length != curSegment
 
-        // Add the consumed segment into parsed as consumedParsed
-        for (i in 0 until consumedCurSegment) {
-            consumedParsed += '#'
+            // This segment is consumed! Onward and upward
+            segments.removeFirst()
+            sum += processLine(parsed + "#".repeat(curSegment) + (if (notEnd) '.' else ""),
+                    line.substring(curSegment + (if (notEnd) 1 else 0)),
+                    segments)
+            // sum += processLine(consumedParsed, line.substring(consumedCurSegment), segments)
+            segments.add(0, curSegment)
         }
-
-        // Either we're at the end or the next one is a '.'
-        if (line.length != consumedCurSegment) {
-            // If we're not at the end, that means we need the next character to be a '.'
-            if (line[consumedCurSegment] == '#') {
-                // Ruh-roh, we're forced to have a broken one? Failed attempt
-                return 0
-            }
-            consumedParsed += '.'
-            consumedCurSegment++
-        }
-
-        // This segment is consumed! Onward and upward
-        segments.removeFirst()
-        sum += processLine(consumedParsed, line.substring(consumedCurSegment), segments)
-        segments.add(0, curSegment)
     }
-    // Always try not inserting and move along
-    sum += processLine(parsed + '.', line.substring(1), segments)
+
+    // Try not inserting and move along
+    if (line[0] != '#') {
+        sum += processLine(parsed + '.', line.substring(1), segments)
+    }
 
     return sum
 }
 
 fun main() {
-    println("part1: " + part1())
+    println("part1: " + part1("day12.txt"))
 }
