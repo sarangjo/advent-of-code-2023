2e88c16006747d992fc0b96fcaf57760d2f5f6e5
Sarang Joshi <sarangj@msn.com>
2024-02-26 10:26:42 -0600

    Part 1 working for sample

--- day13.lua
+++ day13.lua
@@ -1,71 +1,119 @@
-function get_bitwise(mirrors)
-    local s = ''
-    for i = 1, 32, 1 do
-        if bit32.band(mirrors, bit32.lshift(1, i)) then
-            if not s == '' then
-                s = s .. ','
+---
+function update_vertical_mirrors(line, vmirrors)
+    for i, mirror in pairs(vmirrors) do
+        --- check if this could be the mirror
+        local j = 0
+        local still_mirror = true
+        while mirror - j >= 1 and mirror + 1 + j <= string.len(line) do
+            if string.sub(line, mirror - j, mirror - j) ~= string.sub(line, mirror + 1 + j, mirror + 1 + j) then
+                still_mirror = false
+                break
             end
-            s = s .. i
+            j = j + 1
+        end
+        if not still_mirror then
+            vmirrors[i] = nil
         end
     end
-    return s
+
+    return vmirrors
 end
 
----
-function update_vertical_mirrors(line, mirrors)
-    for mirror = 1, string.len(line) do
-        --- check if this could be the mirror
-        --- no continues in Lua...
-        if bit32.band(mirrors, bit32.lshift(1, mirror)) then
-            local i = 0
-            local still_mirror = true
-            while mirror - i >= 1 and mirror + 1 + i <= string.len(line) do
-                if not string.sub(line, mirror - i, mirror - i) == string.sub(line, mirror + 1 + i, mirror + 1 + i) then
-                    break
-                end
+function update_horizontal_mirrors(pattern, col, hmirrors)
+    for i, mirror in pairs(hmirrors) do
+        local j = 0
+        local still_mirror = true
+        while mirror - j >= 1 and mirror + 1 + j <= #pattern do
+            if string.sub(pattern[mirror - j], col, col) ~= string.sub(pattern[mirror + 1 + j], col, col) then
+                still_mirror = false
+                break
             end
-            if not still_mirror then
-                mirrors = bit32.band(mirrors, bit32.bnot(bit32.lshift(1, mirror)))
-            end
+            j = j + 1
+        end
+        if not still_mirror then
+            hmirrors[i] = nil
         end
     end
 
-    return mirrors
+    return hmirrors
+end
+
+function mirror_count(mirrors)
+    local count = 0
+    for _ in pairs(mirrors) do
+        count = count + 1
+    end
+    return count
 end
 
 function part1(file)
-    local f = io.input(file)
+    io.input(file)
+    local score = 0
 
     repeat
         local pattern = {}
-        local i = 1
+        local line_length = 0
 
-        local line = f.read("*l")
+        -- Fencepost to see line length
+        local line = io.read("*l")
+        line_length = string.len(line)
 
-        local mirrors = 2 ^ string.len(line) - 1
-
-        print("mirrors", get_bitwise(mirrors))
+        -- Set up possible vertical mirrors
+        local vmirrors = {}
+        for j = 1, line_length - 1, 1 do
+            vmirrors[j] = j
+        end
+        local might_have_vertical_mirror = true
 
         while true do
-            --- check if this line has vertical mirror
-            os.exit()
+            pattern[#pattern + 1] = line
 
-            line = f.read("*l")
+            if might_have_vertical_mirror then
+                update_vertical_mirrors(line, vmirrors)
+            end
 
-            if line == '' then
+            -- check if this line has vertical mirror
+            if mirror_count(vmirrors) == 0 then
+                -- no possible vertical mirror
+                might_have_vertical_mirror = false
+            end
+
+            line = io.read("*l")
+
+            if line == '' or not line then
                 break
             end
         end
-    until not s
 
-    for line in io.lines(file) do
-        --- We process line by line but also store it in a table so we can check for a potential
-        --- horizontal mirror
-        lines[#lines + 1] = line
-    end
+        -- did we find a vertical mirror?
+        if might_have_vertical_mirror and mirror_count(vmirrors) == 1 then
+            -- found it! there's only one so go into it
+            for k in pairs(vmirrors) do
+                score = score + k
+            end
+        else
+            -- okay now time for horizontal checking; we have all of the lines loaded so repeat the process
+            local hmirrors = {}
+            for j = 1, #pattern - 1, 1 do
+                hmirrors[j] = j
+            end
 
-    --- check for horizontal line
+            for col = 1, line_length, 1 do
+                -- check this column
+                update_horizontal_mirrors(pattern, col, hmirrors)
+
+                -- we're guaranteed to have a mirror so once we're down to 1 immediately bop out
+                if mirror_count(hmirrors) == 1 then
+                    for k in pairs(hmirrors) do
+                        score = score + (k * 100)
+                    end
+                    break
+                end
+            end
+        end
+
+        print("score:", score)
+    until not line
 end
 
--- part1("sample13.txt")
-print(get_bitwise(7))
+part1("sample13.txt")
