9d6859a6dcf6f8004f50011867d07c4795788c85
Sarang Joshi <sarangj@msn.com>
2024-02-28 21:15:52 -0600

    To recurse or not to recurse...

--- day14.rkt
+++ day14.rkt
@@ -1,36 +1,35 @@
 #lang racket
 
 ; returns line-number, total-weight, rock-count
-(define (process-lines file line-number total-weight rock-count)
+(define (process-lines file rock-positions line-number total-weight rock-count)
     ; scoped definition of the variable line, initialized to the first line
     (let ((line (read-line file 'any)))
-        ; implicit base case
         (if (eof-object? line)
+            ; implicit base case
             (values 0 0 0)
-            ; here we process line by line
-            (for ([i (in-naturals)]
-                  [c (in-string line)])
-                ())
-            (displayln line)
-            (displayln line-number)
-            (process-lines file (+ 1 line-number)))))
+            (begin
+                ; here we process line by line
+                (for ([col (in-naturals)]
+                      [c (in-string line)])
+                    ; process each character
+                    (cond
+                        [(= c #\O)
+                            ; bump this position, register this rock
+                        ]
+                        )
+                    (printf "Char: ~a\n" c))
+                (process-lines file rock-positions (+ 1 line-number) total-weight rock-count)))))
 
 (define (part1 filename)
     ; set up rock positions as 0 for each column
     (define rock-positions (make-vector (string-length (call-with-input-file filename
-                            (lambda (in)
-                            (read-line in)))) 0))
-    (display (vector-length rock-positions)))
+                (lambda (in)
+                (read-line in)))) 0))
+    (printf "Vector length: ~a\n" (vector-length rock-positions))
 
-    ; iterate through each line and accumulate
+    ; start recursion
     (define in (open-input-file filename))
-    (for/fold ([total-weight 0]
-               [rock-count 0])
-              ([line-number (in-naturals)]
-
-               )
-
-        )
-    ; better than recursive?
+    (printf "Result: ~a\n" (process-lines in rock-positions 0 0 0))
+)
 
 (part1 "sample14.txt")
