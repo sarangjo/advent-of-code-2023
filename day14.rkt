72ec003cf3f556d613b2b98eddbe54b1810e4661
Sarang Joshi <sarangj@msn.com>
2024-02-28 18:31:43 -0600

    Hmmmmm

--- day14.rkt
+++ day14.rkt
@@ -1,19 +1,19 @@
 #lang racket
 
-(define total-weight 0)
-(define rock-count 0)
-
-; (define (process-lines file line-number)
-;     ; scoped definition of the variable line, initialized to the first line
-;     (let ((line (read-line file 'any)))
-;         (unless (eof-object? line)
-;             ; here we process line by line
-;             (for ([i (in-naturals)]
-;                   [c (in-string line)])
-;                 ())
-;             (displayln line)
-;             (displayln line-number)
-;             (process-lines file (+ 1 line-number)))))
+; returns line-number, total-weight, rock-count
+(define (process-lines file line-number total-weight rock-count)
+    ; scoped definition of the variable line, initialized to the first line
+    (let ((line (read-line file 'any)))
+        ; implicit base case
+        (if (eof-object? line)
+            (values 0 0 0)
+            ; here we process line by line
+            (for ([i (in-naturals)]
+                  [c (in-string line)])
+                ())
+            (displayln line)
+            (displayln line-number)
+            (process-lines file (+ 1 line-number)))))
 
 (define (part1 filename)
     ; set up rock positions as 0 for each column
@@ -21,5 +21,16 @@
                             (lambda (in)
                             (read-line in)))) 0))
     (display (vector-length rock-positions)))
+
+    ; iterate through each line and accumulate
+    (define in (open-input-file filename))
+    (for/fold ([total-weight 0]
+               [rock-count 0])
+              ([line-number (in-naturals)]
+
+               )
+
+        )
+    ; better than recursive?
 
 (part1 "sample14.txt")
