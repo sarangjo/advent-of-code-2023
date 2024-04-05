1dd7eabaf69d2560df31bda9e280664e78d030e2
Sarang Joshi <sarangj@msn.com>
2024-04-05 02:44:40 -0500

    Working day 14 part 2

--- day14.rkt
+++ day14.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require racket/set)
+
 (define (part1 filename)
     ; set up rock positions as 0 for each column
     (define rock-positions (make-vector (string-length (call-with-input-file filename
@@ -46,7 +48,7 @@
             (values (add1 line-number) (+ line-weight total-weight) (+ line-rock-count rock-count))
     ))
 
-    (printf "Line count ~a, Total weight ~a, rock count ~a\n" total-lines total-weight total-rock-count)
+    ; (printf "Line count ~a, Total weight ~a, rock count ~a\n" total-lines total-weight total-rock-count)
 
     ; We're calculating the weight by increasing line numbers 0...n-1; so to calculate actual weight
     ; we subtract from n*rock-count since each rock's weight is (n-w)
@@ -58,9 +60,7 @@
         (printf "~a\n" row))
     (printf "\n"))
 
-(define (cycle grid)
-    (print-grid grid)
-
+(define (do-cycle grid)
     ; Move north
     (for ([j (in-range (vector-length grid))])
         (for/fold ([destination 0]) ([i (in-naturals)] [row (in-vector grid)])
@@ -74,8 +74,6 @@
                     (add1 i)]
                 [else destination])))
 
-    (print-grid grid)
-
     ; Move west
     (for ([i (in-naturals)] [row (in-vector grid)])
         (for/fold ([destination 0]) ([j (in-naturals)] [cell (in-string row)])
@@ -88,8 +86,6 @@
                 [(char=? cell #\#)
                     (add1 j)]
                 [else destination])))
-
-    (print-grid grid)
 
     ; Move south
     (for ([j (in-range (vector-length grid))])
@@ -105,8 +101,6 @@
                     (sub1 i)]
                 [else destination])))
 
-    (print-grid grid)
-
     ; Move east
     (for ([i (in-naturals)] [row (in-vector grid)])
         (for/fold ([destination (sub1 (vector-length grid))]) ([j (in-range (sub1 (vector-length grid)) -1 -1)])
@@ -120,12 +114,59 @@
                     (sub1 j)]
                 [else destination])))
 
-    (print-grid grid)
-
     ; Don't return anything... or return grid maybe?
     (void))
 
+(define (get-rock-locs grid)
+    (for*/set ([i (in-range (vector-length grid))]
+               [j (in-range (string-length (vector-ref grid i)))]
+               #:when (char=? (string-ref (vector-ref grid i) j) #\O))
+        (cons i j)))
+
+(define (calculate-load rock-locs row-count)
+    (for/fold ([sum 0]) ([p (in-set rock-locs)]) (+ sum (- row-count (car p)))))
+
 (define (part2 filename)
-    ; (printf "part1: ~a\n" (part1 "day14.txt"))
-    (define grid (list->vector (file->lines "sample14.txt")))
-    (cycle grid))
+    (define grid (list->vector (file->lines filename)))
+
+    ; set up first cycle
+    (do-cycle grid)
+
+    ; we don't have a clean list comprehension approach:
+    ; - possible-cycle gets set only inside the inner for loop for the break condition
+    (define possible-cycle 0)
+    ; - full-rock-locs would be nice to accumulate, but we need to reference this list from inside the loop
+    (define full-rock-locs (list (get-rock-locs grid)))
+
+    ; while loop
+    (for ([cycle (in-naturals 2)])
+        ; body
+        (do-cycle grid)
+        (define rock-locs (get-rock-locs grid))
+
+        ; (printf "Cycles completed: ~a. Full len ~a\n" cycle (length full-rock-locs))
+
+        ; are we here?
+        #:break (
+            for/or ([i (in-range (sub1 cycle))])
+                ; body
+                (if (equal? (list-ref full-rock-locs (- cycle i 2)) rock-locs)
+                    (begin (set! possible-cycle (add1 i)) #t)
+                    #f))
+
+        (set! full-rock-locs (append full-rock-locs (list rock-locs)))
+    )
+
+    ; (printf "possible cycle ~a\n" possible-cycle)
+
+    ; now that we have the cycle, what's the offset into our full-rock-locs that our cycle begins?
+    (define offset (- (length full-rock-locs) possible-cycle 1))
+    ; cut off the offset from our overall 10^9 target
+    (define adjusted-target (- 1000000000 offset))
+    ; now, calculate the cycle within our range (in full-rock-locs) that matches our target
+    (define match (remainder adjusted-target possible-cycle))
+    ; calculate the load for that cycle (sub1 because we start at cycle 2)
+    (calculate-load (list-ref full-rock-locs (sub1 (+ offset match))) (vector-length grid)))
+
+; (printf "part1: ~a\n" (part1 "day14.txt"))
+(printf "part2: ~a\n" (part2 "day14.txt"))
