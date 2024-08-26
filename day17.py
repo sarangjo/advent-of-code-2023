a087fa3289a7de1416a0bf77398530a76c85a268
Sarang Joshi <sarangj@msn.com>
2024-08-26 18:35:22 -0500

    Start djikstra's but needs work

--- day17.py
+++ day17.py
@@ -1,71 +1,36 @@
-def move(pos, dir):
-    if dir == 'R':
-        return (pos[0], pos[1] + 1)
-    elif dir == 'D':
-        return (pos[0] + 1, pos[1])
-    elif dir == 'L':
-        return (pos[0], pos[1] - 1)
-    else:  # 'U'
-        return (pos[0] - 1, pos[1])
-
-
-dirs = {
-    'X': ['R', 'D'],
-    'R': ['U', 'R', 'D'],
-    'D': ['R', 'D', 'L'],
-    'L': ['D', 'L', 'U'],
-    'U': ['L', 'U', 'R']
-}
-
-
-def get_best_weight(grid, pos, path, same_dir_count, dir):
-    if pos[0] < 0 or pos[0] >= len(grid) or pos[1] < 0 or pos[1] >= len(grid) or path[pos[0]][pos[1]]:
-        return -1
-
-    possible_directions = dirs[dir]
-    best_weight = -1
-
-    if pos[0] == len(grid)-1 and pos[1] == len(grid) - 1:
-        return grid[pos[0]][pos[1]]
-
-    path[pos[0]][pos[1]] = True
-
-    for next_d in possible_directions:
-        if next_d == dir:
-            same_dir_count += 1
-        else:
-            same_dir_count = 0
-
-        if same_dir_count >= 4:
-            continue
-
-        next_weight = get_best_weight(grid, move(
-            pos, next_d), path, same_dir_count, next_d)
-        if next_weight < 0:
-            continue
-
-        if best_weight < 0 or next_weight < best_weight:
-            best_weight = next_weight
-
-    path[pos[0]][pos[1]] = False
-
-    return best_weight if best_weight < 0 else grid[pos[0]][pos[1]] + best_weight
+#!/usr/bin/env python
+import math
+from queue import PriorityQueue
 
 
 def main():
     with open("sample17.txt") as f:
         lines = f.readlines()
 
-    grid = [[int(c) for c in line.strip()] for line in lines]
+    # 1. Set of all unvisited nodes
+    unvisited = PriorityQueue()
+    visited = {}
 
-    print(grid)
+    for i in range(len(lines)):
+        for j in range(len(lines)):
+            unvisited.put((0 if i == 0 and j == 0 else math.inf, (i, j)))
 
-    r_weight = get_best_weight(grid, (0, 1), [[False for _ in range(
-        len(grid))] for _ in range(len(grid))], 0, 'R')
-    d_weight = get_best_weight(grid, (1, 0), [[False for _ in range(
-        len(grid))] for _ in range(len(grid))], 0, 'D')
+    while True:
+        cur = unvisited.get()
 
-    print("r", r_weight, "d", d_weight)
+        if not cur or cur[0] == math.inf:
+            break
+
+        # Consider all neighbors
+        neighbors = []
+        # - U
+        if cur[1][0] != 0 and (cur[1][0] - 1, cur[1][1]) not in visited:
+            neighbors.append()
+
+        # Mark as visited
+        visited[cur[1]] = True
+
+
 
 
 if __name__ == "__main__":
