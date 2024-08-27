01a1de712fa4e326c75f06566e9e9a1e96ce1ec1
Sarang Joshi <sarangj@msn.com>
2024-08-27 03:31:33 -0500

    Implement min-heap with necessary functions

--- day17.py
+++ day17.py
@@ -1,9 +1,116 @@
 #!/usr/bin/env python
 import math
 from queue import PriorityQueue
+from typing import Dict, List, Tuple
+
+class HeapItem:
+    loc: Tuple[int, int]
+    priority: int
+
+    def __init__(self, loc: Tuple[int, int], priority: int) -> None:
+        self.loc = loc
+        self.priority = priority
+
+    def __str__(self) -> str:
+        return f"({self.loc[0]}, {self.loc[1]}): {self.priority}"
+
+    def __repr__(self) -> str:
+        return str(self)
 
 
-def main():
+# Let's build our own heap that has efficient operations that we care about: insert, pop, update
+class MinHeap:
+    # Heap is backed by an array
+    _elements: List[HeapItem] = []
+
+    # Index into the array is identified by this dict
+    _indexMap: Dict[Tuple, int] = {}
+
+    def _parent(idx: int) -> int:
+        return int((idx - 1) / 2)
+
+    def _children(idx: int) -> Tuple[int]:
+        return (2*idx + 1, 2*idx + 2)
+
+    def _swap(self, a: int, b: int):
+        # Swap idx and parent
+        temp = self._elements[a]
+        self._elements[a] = self._elements[b]
+        self._elements[b] = temp
+
+        # Update indexMap
+        self._indexMap[self._elements[a].loc] = a
+        self._indexMap[self._elements[b].loc] = b
+
+    def _bubble_up(self, idx: int):
+        # Is the child smaller than the parent?
+        parent = MinHeap._parent(idx)
+
+        while idx != parent and parent >= 0 and parent < len(self._elements) and self._elements[parent].priority > self._elements[idx].priority:
+            # Swap idx and parent
+            self._swap(idx, parent)
+
+            idx = parent
+            parent = MinHeap._parent(idx)
+
+    def insert(self, item: HeapItem):
+        # Add element at the end
+        self._elements.append(item)
+        idx = len(self._elements) - 1
+        self._indexMap[item.loc] = idx
+
+        # Bubble up!
+        self._bubble_up(idx)
+
+    def _bubble_down(self, idx: int):
+        left, right = MinHeap._children(idx)
+
+        while True:
+            # Check children
+            smallest = idx
+
+            if left < len(self._elements) and self._elements[left].priority < self._elements[smallest].priority:
+                smallest = left
+            if right < len(self._elements) and self._elements[right].priority < self._elements[smallest].priority:
+                smallest = right
+
+            if smallest == idx:
+                break
+
+            self._swap(idx, smallest)
+
+            idx = smallest
+            left, right = MinHeap._children(idx)
+
+    def pop(self):
+        ret = self._elements[0]
+
+        # Insert last element into front and bubble down
+        new_root = self._elements.pop()
+        self._elements[0] = new_root
+        del self._indexMap[ret.loc]
+        self._indexMap[new_root.loc] = 0
+
+        # Is the parent larger than the child?
+        self._bubble_down(0)
+
+        return ret
+
+    def decrease_priority(self, loc: Tuple, p: int) -> bool:
+        try:
+            idx = self._indexMap[loc]
+        except:
+            return False
+
+        if p >= self._elements[idx].priority:
+            return False
+
+        # Reduce priority and bubble up
+        self._elements[idx].priority = p
+        self._bubble_up(idx)
+
+
+def main2():
     with open("sample17.txt") as f:
         lines = f.readlines()
 
@@ -31,7 +138,17 @@ def main():
         visited[cur[1]] = True
 
 
+def main():
+    h = MinHeap()
 
+    h.insert(HeapItem((0, 0), 4))
+    h.insert(HeapItem((0, 1), 7))
+    h.insert(HeapItem((1, 1), 2))
+    h.insert(HeapItem((1, 0), 3))
+
+    h.decrease_priority((0, 1), 1)
+
+    print(h.pop())
 
 if __name__ == "__main__":
     main()
